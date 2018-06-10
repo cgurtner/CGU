1699c11081c78b53ba4501b59c1ac6bd8f87bce4
Cyrill Gurtner <i@cygu.ch>
2018-06-10 14:39:26 +0200

    home icons order

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -8,12 +8,12 @@
                         <h2 class="home">Software Developer<span class="blink">_</span></h2>
                     </header>
                     <div class="social-links">
-                        <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf GitLab"
-                           href="https://gitlab.com/cygu" target="_blank"><i class="fab fa-gitlab"></i></a>
                         <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Facebook"
                            href="https://www.facebook.com/cyguCH" target="_blank"><i class="fab fa-facebook"></i></a>
                         <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Instagram"
                            href="https://www.instagram.com/cygu_" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf GitLab"
+                           href="https://gitlab.com/cygu" target="_blank"><i class="fab fa-gitlab"></i></a>
                     </div>
                     <div class="rsa-fingerprint">
                         <a href="files/id_rsa.pub.txt">59:01:f2:63:42:19:5b:59:1e:13:35:f1:96:ca:f2:e5</a>
