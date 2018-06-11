4264f8fac7558a32cf4c1fb3ab6a80c4b176ea36
Cyrill Gurtner <i@cygu.ch>
2018-06-11 20:51:44 +0200

    remove blog.rss link

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -8,15 +8,13 @@
                         <h2 class="home">Software Developer<span class="blink">_</span></h2>
                     </header>
                     <div class="social-links">
-                        <a data-toggle="tooltip" data-placement="bottom" title="Ich auf GitLab"
-                           href="<?php echo CONFIG['gitlab']; ?>" target="_blank"><i class="fab fa-gitlab"></i></a>
                         <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Facebook"
                            href="<?php echo CONFIG['facebook']; ?>" target="_blank"><i class="fab fa-facebook"></i></a>
                         <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Instagram"
-                           href="<?php echo CONFIG['instagram']; ?>" target="_blank"><i class="fab fa-instagram"></i></a>
-                        <a data-toggle="tooltip" data-placement="bottom" title="Blog RSS"
-                           href="<?php echo CONFIG['baseURL']; ?>blog.rss" target="_blank"><i
-                                    class="fas fa-rss"></i></a>
+                           href="<?php echo CONFIG['instagram']; ?>" target="_blank"><i
+                                    class="fab fa-instagram"></i></a>
+                        <a data-toggle="tooltip" data-placement="bottom" title="Ich auf GitLab"
+                           href="<?php echo CONFIG['gitlab']; ?>" target="_blank"><i class="fab fa-gitlab"></i></a>
                     </div>
                     <div class="rsa-fingerprint">
                         <a href="files/id_rsa.pub.txt">59:01:f2:63:42:19:5b:59:1e:13:35:f1:96:ca:f2:e5</a>
