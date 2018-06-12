47aea461144804a47499a02fb63872e5424db963
Cyrill Gurtner <i@cygu.ch>
2018-06-12 15:20:15 +0200

    move home.php name to left

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -1,11 +1,11 @@
 <main>
-    <div class="container-fluid">
+    <div class="container">
         <section>
             <div class="row">
                 <div class="col pt-5">
                     <header>
-                        <h1 class="home">Cyrill Gurtner</h1>
-                        <h2 class="home">Software Developer<span class="blink">_</span></h2>
+                        <h1>Cyrill Gurtner</h1>
+                        <h2>Software Developer<span class="blink">_</span></h2>
                     </header>
                     <div class="social-links">
                         <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Facebook"
