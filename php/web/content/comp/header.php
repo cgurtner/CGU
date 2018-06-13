3ee09a1a4b4e3ad9456c309d2edfdc719b0dca56
Cyrill Gurtner <i@cygu.ch>
2018-06-13 22:37:43 +0200

    google structured breadcrumbs

--- php/web/content/comp/header.php
+++ php/web/content/comp/header.php
@@ -1,11 +1,28 @@
 <?php
 
-$breadLinks = '<li class="breadcrumb-item"><a href="home">Home</a></li>';
+$breadLinks = '<li class="breadcrumb-item" property="itemListElement" typeof="ListItem">';
+$breadLinks .= '<a property="item" typeof="WebPage" href="home">Home</a>';
+$breadLinks .= '<meta property="position" content="1">';
+$breadLinks .= '</li>';
 if ($page['type'] === 'blog') {
-    $breadLinks .= '<li class="breadcrumb-item"><a href="blog">Blog</a></li>';
-    $breadLinks .= '<li class="breadcrumb-item active" aria-current="page">' . $page['title'] . '</li>';
+    $breadLinks .= '<li class="breadcrumb-item" property="itemListElement" typeof="ListItem">';
+    $breadLinks .= '<a property="item" typeof="WebPage" href="blog">Blog</a>';
+    $breadLinks .= '<meta property="position" content="2">';
+    $breadLinks .= '</li>';
+
+    $breadLinks .= '<li class="breadcrumb-item active" aria-current="page" property="itemListElement" typeof="ListItem">';
+    $breadLinks .= '<a href="' . $page['url'] . '" property="item" typeof="WebPage">';
+    $breadLinks .= $page['title'];
+    $breadLinks .= '</a>';
+    $breadLinks .= '<meta property="position" content="3">';
+    $breadLinks .= '</li>';
 } else {
-    $breadLinks .= '<li class="breadcrumb-item active" aria-current="page">' . $page['title'] . '</li>';
+    $breadLinks .= '<li class="breadcrumb-item active" aria-current="page" property="itemListElement" typeof="ListItem">';
+    $breadLinks .= '<a href="' . $page['url'] . '" property="item" typeof="WebPage">';
+    $breadLinks .= $page['title'];
+    $breadLinks .= '</a>';
+    $breadLinks .= '<meta property="position" content="2">';
+    $breadLinks .= '</li>';
 }
 
 ?>
@@ -14,7 +31,7 @@ if ($page['type'] === 'blog') {
         <div class="col pt-5 h1-box">
             <h1><?php echo $page['title']; ?></h1>
             <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
+                <ol class="breadcrumb" vocab="http://schema.org/" typeof="BreadcrumbList">
                     <?php echo $breadLinks; ?>
                 </ol>
             </nav>
