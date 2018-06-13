80d5442893c0677efe168d74bb680a787200cac2
Cyrill Gurtner <i@cygu.ch>
2018-06-13 22:24:34 +0200

    inline google structural content

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -7,14 +7,20 @@
                         <h1>Cyrill Gurtner</h1>
                         <h2>Software Developer<span class="blink">_</span></h2>
                     </header>
-                    <div class="social-links">
+                    <div class="social-links" itemscope itemtype="http://schema.org/Person">
+                        <link itemprop="url" href="<?php echo CONFIG['baseURL']; ?>">
                         <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Facebook"
-                           href="<?php echo CONFIG['facebook']; ?>" target="_blank"><i class="fab fa-facebook"></i></a>
+                           itemprop="sameAs" href="<?php echo CONFIG['facebook']; ?>" target="_blank">
+                            <i class="fab fa-facebook"></i>
+                        </a>
                         <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Instagram"
-                           href="<?php echo CONFIG['instagram']; ?>" target="_blank"><i
-                                    class="fab fa-instagram"></i></a>
+                           itemprop="sameAs" href="<?php echo CONFIG['instagram']; ?>" target="_blank">
+                            <i class="fab fa-instagram"></i>
+                        </a>
                         <a data-toggle="tooltip" data-placement="bottom" title="Ich auf GitLab"
-                           href="<?php echo CONFIG['gitlab']; ?>" target="_blank"><i class="fab fa-gitlab"></i></a>
+                           itemprop="sameAs" href="<?php echo CONFIG['gitlab']; ?>" target="_blank">
+                            <i class="fab fa-gitlab"></i>
+                        </a>
                     </div>
                     <div class="rsa-fingerprint">
                         <a href="files/id_rsa.pub.txt">59:01:f2:63:42:19:5b:59:1e:13:35:f1:96:ca:f2:e5</a>
