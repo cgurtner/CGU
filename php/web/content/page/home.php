3ade9160453faf0e78ff57c815640cc927f92b07
Cyrill Gurtner <i@cygu.ch>
2018-07-01 18:19:27 +0200

    remove werdegang module accordion

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -10,17 +10,13 @@
                     </header>
                     <div class="social-links" itemscope itemtype="http://schema.org/Person">
                         <link itemprop="url" href="<?php echo CONFIG['baseURL']; ?>">
-                        <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Facebook"
-                           itemprop="sameAs" href="<?php echo CONFIG['facebook']; ?>" target="_blank">
-                            <i class="fab fa-facebook"></i>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="bottom" title="Folge mir auf Instagram"
-                           itemprop="sameAs" href="<?php echo CONFIG['instagram']; ?>" target="_blank">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="bottom" title="Ich auf GitLab"
+                        <a data-toggle="tooltip" data-placement="bottom" title="Mein GitLab"
                            itemprop="sameAs" href="<?php echo CONFIG['gitlab']; ?>" target="_blank">
                             <i class="fab fa-gitlab"></i>
+                        </a>
+                        <a data-toggle="tooltip" data-placement="bottom" title="Mein Instagram"
+                           itemprop="sameAs" href="<?php echo CONFIG['instagram']; ?>" target="_blank">
+                            <i class="fab fa-instagram"></i>
                         </a>
                     </div>
                 </div>
