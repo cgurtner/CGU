6036165f616dbf709b4109480c340a63ff2afe63
Cyrill Gurtner <i@cygu.ch>
2018-07-23 21:09:22 +0200

    fontpage without background image

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -1,8 +1,8 @@
-<main class="fs-bg">
+<main>
     <div class="container">
         <section>
             <div class="row">
-                <div class="col pt-5">
+                <div class="col pt-5 text-center">
                     <header>
                         <h1>Cyrill Gurtner</h1>
                         <h2>Software Developer<span class="blink">_</span></h2>
