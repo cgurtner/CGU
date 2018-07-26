a0d1ed6ae07dc4601681de1261ea082c577a397d
Cyrill Gurtner <i@cygu.ch>
2018-07-26 22:00:28 +0200

    new <h2> styles

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -5,7 +5,7 @@
                 <div class="col pt-5 text-center">
                     <header>
                         <h1>Cyrill Gurtner</h1>
-                        <h2>Software Developer<span class="blink">_</span></h2>
+                        <h2 class="unset-background">Software Developer<span class="blink">_</span></h2>
                     </header>
                     <div class="social-links" itemscope itemtype="http://schema.org/Person">
                         <link itemprop="url" href="<?php echo CONFIG['baseURL']; ?>">
