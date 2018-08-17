799aa42b6257bbccd99b501f8de4b601b1647677
Cyrill Gurtner <i@cygu.ch>
2018-08-17 14:53:10 +0200

    move nav into container

--- php/web/content/comp/nav.php
+++ php/web/content/comp/nav.php
@@ -1,26 +1,46 @@
 <header>
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="home" hreflang="de">Cyrill Gurtner</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <?php
-                foreach (PAGES as $p) {
-                    if ($p['disabled'] || !$p['display']) {
-                        continue;
+        <div class="container">
+            <a class="navbar-brand" href="home" hreflang="de">Cyrill Gurtner</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <?php
+                    $pageCount = countPossiblePages();
+                    $counter = 1;
+                    foreach (PAGES as $p) {
+                        if ($p['disabled'] || !$p['display']) {
+                            continue;
+                        }
+                        $last = '';
+                        if ($counter == $pageCount) {
+                            $last = 'last-nav-item';
+                        }
+                        $active = getActive($p, $page['url']);
+                        echo '<a href="' . $p['url'] . '" hreflang="de" class="nav-item nav-link ' . $active . ' ' . $last . '">' . $p['meta']['title'] . '</a>';
+                        $counter++;
                     }
-                    $active = getActive($p, $page['url']);
-                    echo '<a href="' . $p['url'] . '" hreflang="de" class="nav-item nav-link ' . $active . '">' . $p['meta']['title'] . '</a>';
-                }
-                ?>
+                    ?>
+                </div>
             </div>
         </div>
     </nav>
 </header>
 <?php
+
+function countPossiblePages() {
+    $ret = 0;
+    foreach (PAGES as $p) {
+        if ($p['disabled'] || !$p['display']) {
+            continue;
+        }
+        $ret++;
+    }
+    return $ret;
+}
 
 function getActive($page, $url)
 {
