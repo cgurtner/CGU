ae314eb1b44a708bceebf375181c45d2f0c7a932
Cyrill Gurtner <i@cygu.ch>
2018-08-19 23:13:40 +0200

    remove last-item from left nav

--- php/web/content/comp/nav.php
+++ php/web/content/comp/nav.php
@@ -9,34 +9,31 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <?php
-                    $pageCount = countPossiblePages();
-                    $counter = 1;
                     foreach (PAGES as $p) {
                         if ($p['disabled'] || !$p['display']) {
                             continue;
                         }
-                        $last = '';
-                        if ($counter == $pageCount) {
-                            $last = 'last-nav-item';
-                        }
                         $active = getActive($p, $page['url']);
-                        echo '<a href="' . $p['url'] . '" hreflang="de" class="nav-item nav-link ' . $active . ' ' . $last . '">' . $p['meta']['title'] . '</a>';
-                        $counter++;
+                        echo '<a href="' . $p['url'] . '" hreflang="de" class="nav-item nav-link ' . $active . '">' . $p['meta']['title'] . '</a>';
                     }
                     ?>
                 </div>
                 <ul class="navbar-nav ml-md-auto d-none d-lg-inline-flex">
                     <li class="nav-item">
-                        <a href="<?php echo CONFIG['gitlab']; ?>" class="nav-link" target="_blank"><i class="fab fa-gitlab"></i></a>
+                        <a href="<?php echo CONFIG['gitlab']; ?>" class="nav-link" target="_blank"><i
+                                    class="fab fa-gitlab"></i></a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo CONFIG['instagram']; ?>" class="nav-link last-nav-item" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="<?php echo CONFIG['instagram']; ?>" class="nav-link last-nav-item" target="_blank"><i
+                                    class="fab fa-instagram"></i></a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ml-md-auto d-lg-none">
                     <li class="nav-item">
-                        <a href="<?php echo CONFIG['gitlab']; ?>" target="_blank" class="nav-link" style="display: inline-block;"><i class="fab fa-gitlab"></i></a>
-                        <a href="<?php echo CONFIG['instagram']; ?>" target="_blank" class="nav-link" style="display: inline-block; margin-left: 5px;"><i class="fab fa-instagram"></i></a>
+                        <a href="<?php echo CONFIG['gitlab']; ?>" target="_blank" class="nav-link"
+                           style="display: inline-block;"><i class="fab fa-gitlab"></i></a>
+                        <a href="<?php echo CONFIG['instagram']; ?>" target="_blank" class="nav-link"
+                           style="display: inline-block; margin-left: 5px;"><i class="fab fa-instagram"></i></a>
                     </li>
                 </ul>
             </div>
@@ -44,17 +41,6 @@
     </nav>
 </header>
 <?php
-
-function countPossiblePages() {
-    $ret = 0;
-    foreach (PAGES as $p) {
-        if ($p['disabled'] || !$p['display']) {
-            continue;
-        }
-        $ret++;
-    }
-    return $ret;
-}
 
 function getActive($page, $url)
 {
