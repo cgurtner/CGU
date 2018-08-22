39b92e82c39a9b8681cfff64a5400fb81006cce7
Cyrill Gurtner <i@cygu.ch>
2018-08-22 21:26:42 +0200

    blog datum verschieben

--- php/web/content/page/blog.php
+++ php/web/content/page/blog.php
@@ -16,15 +16,14 @@
                 $html .= '<div class="row">';
                 $html .= '<div class="col-sm-12">';
                 $html .= '<header>';
-                $html .= '<h2>' . $title . '<small class="text-muted">&nbsp;';
-                $html .= '<time datetime="' . $date->format('Y-m-d H:i:s') . '">' . $date->format('d.m.y H:i') . '</time>';
-                $html .= '</small>';
-                $html .= '</h2>';
+                $html .= '<h2>' . $title . '</h2>';
                 $html .= '</header>';
                 $html .= '<p>';
-                $html .= $preview;
+                $html .= $preview . ' &nbsp;';
+                $html .= '<small><time datetime="' . $date->format('Y-m-d H:i:s') . '">' . $date->format('d.m.y H:i') . '</time></small>';
                 $html .= '</p>';
                 $html .= '<a href="' . $page['url'] . '" hreflang="de">Jetzt lesen</a>';
+
                 $html .= '</div>';
                 $html .= '</div>';
                 $html .= '</section>';
