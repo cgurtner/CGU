6755c0174200d58283a8caae000bebc7b01ebbc4
Cyrill Gurtner <i@cygu.ch>
2018-09-11 18:47:22 +0200

    move social links to left

--- php/web/content/comp/nav.php
+++ php/web/content/comp/nav.php
@@ -7,18 +7,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <?php
-                    foreach (PAGES as $p) {
-                        if ($p['disabled'] || !$p['display']) {
-                            continue;
-                        }
-                        $active = getActive($p, $page['url']);
-                        echo '<a href="' . $p['url'] . '" hreflang="de" class="nav-item nav-link ' . $active . '">' . $p['meta']['title'] . '</a>';
-                    }
-                    ?>
-                </div>
-                <ul class="navbar-nav ml-md-auto d-none d-lg-inline-flex">
+                <ul class="navbar-nav d-none d-lg-inline-flex">
                     <li class="nav-item">
                         <a href="<?php echo CONFIG['gitlab']; ?>" class="nav-link" target="_blank" data-toggle="tooltip"
                            data-placement="bottom" title="Mein GitLab"><i
@@ -30,9 +19,19 @@
                                     class="fab fa-instagram"></i></a>
                     </li>
                 </ul>
+                <div class="navbar-nav ml-md-auto">
+                    <?php
+                    foreach (PAGES as $p) {
+                        if ($p['disabled'] || !$p['display']) {
+                            continue;
+                        }
+                        $active = getActive($p, $page['url']);
+                        echo '<a href="' . $p['url'] . '" hreflang="de" class="nav-item nav-link ' . $active . '">' . $p['meta']['title'] . '</a>';
+                    }
+                    ?>
+                </div>
                 <ul class="navbar-nav ml-md-auto d-lg-none">
                     <li class="nav-item">
-
                         <a href="<?php echo CONFIG['gitlab']; ?>" target="_blank" class="nav-link"
                            style="display: inline-block;"><i class="fab fa-gitlab"></i></a>
                         <a href="<?php echo CONFIG['instagram']; ?>" target="_blank" class="nav-link"
