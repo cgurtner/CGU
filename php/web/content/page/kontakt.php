7c068203d11f245b18d7bde9377b24cef774e7e9
Cyrill Gurtner <i@cygu.ch>
2018-09-11 19:08:53 +0200

    add id_rsa.pub

--- php/web/content/page/kontakt.php
+++ php/web/content/page/kontakt.php
@@ -37,6 +37,10 @@ if (isset($contactForm['err']) && !in_array(false, $contactForm['err'])) {
                             <a href="<?php echo CONFIG['baseUrl']; ?>"><?php echo CONFIG['baseUrlAsShown']; ?></a>
                         </p>
                     </address>
+                    <p>
+                        <strong>Cryptographic Data</strong><br>
+                        <a href="gui/file/id_rsa.pub">59:01:f2:63:42:19:5b:59:1e:13:35:f1:96:ca:f2:e5</a>
+                    </p>
                 </div>
                 <div class="col-sm-12 col-md-6">
                     <?php echo $emailSent; ?>
