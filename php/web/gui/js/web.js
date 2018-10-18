19888411ffcb620b8397623fa1bdd3765918f88a
Cyrill Gurtner <i@cygu.ch>
2018-10-18 22:31:50 +0200

    enforce insta-thumb square in jquery

--- php/web/gui/js/web.js
+++ php/web/gui/js/web.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
+
+    const insta = $("img.insta-thumb");
+    insta.outerHeight(insta.outerWidth() * 1);
 });
