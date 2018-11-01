8edb92298ed89898a63d04dad2b9afc8803d51e8
Cyrill Gurtner <i@cygu.ch>
2018-11-01 23:47:29 +0100

    change jquery filename with version number

--- php/web/index.php
+++ php/web/index.php
@@ -55,7 +55,7 @@ if ($page['url'] === 'home') {
     <body>
     <?php include_once("content/comp/nav.php"); ?>
     <?php include_once($page['path']); ?>
-    <script src="gui/js/jquery.js"></script>
+    <script src="gui/js/jquery-3.3.1.js"></script>
     <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="gui/js/web-1.0.0.js"></script>
     </body>
