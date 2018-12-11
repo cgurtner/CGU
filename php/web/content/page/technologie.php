cbcfb505a98faa5fd7c9f47a1c9a8f3a29ab2421
Cyrill Gurtner <i@cygu.ch>
2018-12-11 21:00:55 +0100

    add technologie page

---
+++ php/web/content/page/technologie.php
@@ -0,0 +1,35 @@
+<main>
+    <div class="container">
+        <section>
+            <?php include_once("content/comp/header.php"); ?>
+            <section>
+                <header>
+                    <h2>Title 1</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna non lectus placerat sollicitudin. Cras dignissim mauris id lacus eleifend pretium. Nunc quis sagittis lorem. Proin ac leo vel justo faucibus accumsan at vitae est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa sapien, ultrices a neque eget, ornare luctus justo. Vestibulum hendrerit dui vel magna porttitor auctor. Nam ultrices tincidunt neque sit amet consectetur. Donec blandit dolor suscipit, varius massa quis, tincidunt purus. Nam semper arcu at felis facilisis, vel porta justo condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dui sapien, accumsan a commodo vel, finibus in orci. Nunc cursus nibh arcu, sed finibus sapien hendrerit non. Phasellus nec commodo felis. Integer vitae urna commodo, feugiat magna sed, tincidunt nulla. Sed sollicitudin commodo tortor sit amet consectetur.
+                </p>
+                <p>
+                    Pellentesque feugiat maximus lectus non cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla sodales, eros et pharetra finibus, lacus neque porta lectus, in faucibus nulla ligula sed mi. Donec a interdum lorem. Duis ipsum sem, vehicula vel mi a, luctus tincidunt eros. Fusce a tortor blandit, lobortis dui eu, blandit justo. Sed maximus dui nec lacus auctor, in cursus neque venenatis. Mauris suscipit ex at cursus blandit.
+                </p>
+                <p>
+                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst. Nulla facilisi. Aliquam egestas, mi nec rhoncus commodo, erat sapien pharetra risus, quis semper tortor turpis mattis ante. Praesent sed erat nec lorem blandit pretium. Quisque sit amet felis aliquam leo aliquet viverra. Vestibulum ac imperdiet leo. Vestibulum ut sollicitudin augue.
+                </p>
+            </section>
+            <section>
+                <header>
+                    <h2>Title 2</h2>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna non lectus placerat sollicitudin. Cras dignissim mauris id lacus eleifend pretium. Nunc quis sagittis lorem. Proin ac leo vel justo faucibus accumsan at vitae est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed massa sapien, ultrices a neque eget, ornare luctus justo. Vestibulum hendrerit dui vel magna porttitor auctor. Nam ultrices tincidunt neque sit amet consectetur. Donec blandit dolor suscipit, varius massa quis, tincidunt purus. Nam semper arcu at felis facilisis, vel porta justo condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dui sapien, accumsan a commodo vel, finibus in orci. Nunc cursus nibh arcu, sed finibus sapien hendrerit non. Phasellus nec commodo felis. Integer vitae urna commodo, feugiat magna sed, tincidunt nulla. Sed sollicitudin commodo tortor sit amet consectetur.
+                </p>
+                <p>
+                    Pellentesque feugiat maximus lectus non cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla sodales, eros et pharetra finibus, lacus neque porta lectus, in faucibus nulla ligula sed mi. Donec a interdum lorem. Duis ipsum sem, vehicula vel mi a, luctus tincidunt eros. Fusce a tortor blandit, lobortis dui eu, blandit justo. Sed maximus dui nec lacus auctor, in cursus neque venenatis. Mauris suscipit ex at cursus blandit.
+                </p>
+                <p>
+                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst. Nulla facilisi. Aliquam egestas, mi nec rhoncus commodo, erat sapien pharetra risus, quis semper tortor turpis mattis ante. Praesent sed erat nec lorem blandit pretium. Quisque sit amet felis aliquam leo aliquet viverra. Vestibulum ac imperdiet leo. Vestibulum ut sollicitudin augue.
+                </p>
+            </section>
+        </section>
+    </div>
+</main>
