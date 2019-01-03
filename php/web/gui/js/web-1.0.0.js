5ac2a920f2045c4cb1fce4fbe4192367b05f1d91
Cyrill Gurtner <i@cygu.ch>
2019-01-03 01:31:27 +0100

    readd socials and list them dynamically

--- php/web/gui/js/web-1.0.0.js
+++ php/web/gui/js/web-1.0.0.js
@@ -2,12 +2,4 @@ $(document).ready(function () {
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
-
-    const insta = $("img.insta-thumb");
-    const resizeInsta = () => {
-        insta.outerHeight(insta.outerWidth() * 1);
-    };
-
-    resizeInsta();
-    $(window).resize(resizeInsta);
 });
