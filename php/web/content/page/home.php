a84b33901b60743a62762fabc2f55953e5341cdc
Cyrill Gurtner <i@cygu.ch>
2019-01-03 16:35:16 +0100

    rearrange home.php

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -4,7 +4,8 @@
             <div class="row pt-5">
                 <div class="col-12 col-sm-6 col-md-5 text-center">
                     <header>
-                        <img src="../../gui/image/logo-no-name.png" style="width: 85%" class="img-fluid" alt="Logo von Cyrill Gurtner">
+                        <img src="../../gui/image/logo-no-name.png" style="width: 85%" class="img-fluid"
+                             alt="Logo von Cyrill Gurtner">
                         <h1 style="letter-spacing: unset; font-size: 37px;"><span
                                     style="font-weight: 300;">Cyrill</span>&nbsp;<span
                                     style="font-weight: 600;">Gurtner</span></h1>
@@ -26,35 +27,16 @@
                         lobortis maximus, auctor eget justo. Mauris sit amet ipsum sed augue facilisis rhoncus vel
                         at ex.
                     </p>
+                </div>
+                <div class="col-12">
                     <div class="row ml-0 mr-0 mb-4 mt-4 pt-3 pb-3 dark-card">
-                        <div class="col-6 col-sm-4">
-                            <img src="../../gui/image/portrait.jpg" class="img-fluid rounded-circle" alt="Foto von Cyrill Gurtner">
+                        <div class="col-12 col-sm-6 col-md-5 text-center">
+                            <img src="../../gui/image/portrait.jpg" class="img-fluid rounded-circle mb-3 mb-md-0"
+                                 alt="Foto von Cyrill Gurtner" style="width: 200px;">
                         </div>
-                        <div class="col-6 col-sm-8">
+                        <div class="col-12 col-sm-6 col-md-7">
                             <p>
-                            Morbi quis auctor mauris, a ultrices ante.
-                            Sed libero ante, semper quis mollis non, fermentum at tellus. Sed nisl nulla, facilisis ac
-                            lobortis maximus, auctor eget justo. Mauris sit amet ipsum sed augue facilisis rhoncus vel
-                            at ex.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam augue sit
-                                amet
-                                nulla consequat tempus. Vivamus eu posuere felis, eget imperdiet libero. Mauris interdum
-                                turpis eget feugiat tincidunt. Proin luctus arcu non diam imperdiet hendrerit. Proin
-                                tempus,
-                                magna quis convallis faucibus, magna augue vulputate libero, ut malesuada sem diam
-                                blandit
-                                ligula. In auctor nisi vel nibh tristique, a tempor ante gravida. Lorem ipsum dolor sit
-                                amet, consectetur adipiscing elit. Nullam dui turpis, bibendum at ante ac, vestibulum
-                                rhoncus diam. Aenean vitae libero ac leo imperdiet finibus hendrerit vitae arcu. In
-                                tincidunt felis a neque finibus, sed aliquet augue tempus. Maecenas convallis magna et
-                                lectus commodo, sit amet facilisis nunc gravida. Morbi quis auctor mauris, a ultrices
-                                ante.
+                                Morbi quis auctor mauris, a ultrices ante.
                                 Sed libero ante, semper quis mollis non, fermentum at tellus. Sed nisl nulla, facilisis
                                 ac
                                 lobortis maximus, auctor eget justo. Mauris sit amet ipsum sed augue facilisis rhoncus
