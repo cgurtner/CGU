ac4d0f8ee8a759cddb5285a29875d5745bb0515b
Cyrill Gurtner <i@cygu.ch>
2019-01-19 21:58:14 +0100

    readd gallery

--- php/web/gui/js/web-1.0.0.js
+++ php/web/gui/js/web-1.0.0.js
@@ -3,4 +3,11 @@ $(document).ready(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
     $('main.fade-in').fadeIn(750);
+    const insta = $("img.insta-thumb");
+    const resizeInsta = () => {
+        insta.outerHeight(insta.outerWidth() * 1);
+    };
+
+    resizeInsta();
+    $(window).resize(resizeInsta);
 });
