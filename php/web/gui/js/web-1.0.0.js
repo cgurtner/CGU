ada543a4512456038227973091b072d1684b2f50
Cyrill Gurtner <i@cygu.ch>
2019-01-23 23:40:04 +0100

    fix mobile full sized background image

--- php/web/gui/js/web-1.0.0.js
+++ php/web/gui/js/web-1.0.0.js
@@ -5,6 +5,11 @@ $(document).ready(function () {
 
     $('main.fade-in').fadeIn(750);
 
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
     const insta = $("img.insta-thumb");
     const resizeInsta = () => {
         insta.outerHeight(insta.outerWidth() * 1);
@@ -12,7 +17,7 @@ $(document).ready(function () {
     resizeInsta();
     $(window).resize(resizeInsta);
 
-    $("#scroll-to-content").click(function(e) {
+    $("#scroll-to-content").click(function (e) {
         e.preventDefault();
         $('html, body').animate({scrollTop: $('main div.bg').height() - $('nav').height() - 15}, 750);
     });
