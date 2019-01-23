cdeb8480a4b329f68ed29d2fd30ff4a9e912c449
Cyrill Gurtner <i@cygu.ch>
2019-01-23 21:43:13 +0100

    make logo footer opacity .5

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -1,7 +1,7 @@
 <main class="fade-in">
     <header>
         <div class="bg">
-            <div class="pt-3" style="position: absolute; bottom: 0rem; left: 0; width: 100%; text-align: center; background-color: rgba(250, 250, 250, .25); height: 10rem;">
+            <div class="pt-3" style="position: absolute; bottom: 0rem; left: 0; width: 100%; text-align: center; background-color: rgba(250, 250, 250, .5); height: 10rem;">
                 <img src="../../gui/image/logo-no-name.png" class="img-fluid"  style="height: 65%;"/>
                 <h1 style="letter-spacing: unset; font-size: 37px;"><span
                             style="font-weight: 300;">Cyrill</span>&nbsp;<span
