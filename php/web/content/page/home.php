2babf3bb56d3485859066131282605ae7d6f3844
Cyrill Gurtner <i@cygu.ch>
2019-01-27 19:23:42 +0100

    navbar left padding on mobile

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -9,43 +9,14 @@
             </div>
         </div>
     </header>
-    <div class="container">
+    <div class="container mt-5">
         <section>
             <div class="row">
-                <div class="col-12">
-                    <h2 class="text-center text-sm-left mb-0">Hi,</h2>
+                <div class="col-sm-12 col-md-10">
+                    <h2 class="text-center text-sm-left">Hi,</h2>
                     <p class="mb-0">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam augue sit amet
-                        nulla consequat tempus. Vivamus eu posuere felis, eget imperdiet libero. Mauris interdum
-                        turpis eget feugiat tincidunt. Proin luctus arcu non diam imperdiet hendrerit. Proin tempus,
-                        magna quis convallis faucibus, magna augue vulputate libero, ut malesuada sem diam blandit
-                        ligula. In auctor nisi vel nibh tristique, a tempor ante gravida. Lorem ipsum dolor sit
-                        amet, consectetur adipiscing elit. Nullam dui turpis, bibendum at ante ac, vestibulum
-                        rhoncus diam. Aenean vitae libero ac leo imperdiet finibus hendrerit vitae arcu. In
-                        tincidunt felis a neque finibus, sed aliquet augue tempus. Maecenas convallis magna et
-                        lectus commodo, sit amet facilisis nunc gravida. Morbi quis auctor mauris, a ultrices ante.
-                        Sed libero ante, semper quis mollis non, fermentum at tellus. Sed nisl nulla, facilisis ac
-                        lobortis maximus, auctor eget justo. Mauris sit amet ipsum sed augue facilisis rhoncus vel
-                        at ex.
+                        Ich, Cyrill Gurtner, bin ein $age jähriger Softwareentwickler aus der Schweiz.
                     </p>
-                </div>
-                <div class="col-12">
-                    <div class="row ml-0 mr-0 mb-4 mt-4 pt-3 pb-3 dark-card">
-                        <div class="col-12 col-sm-6 col-md-5 text-center">
-                            <img src="../../gui/image/portrait.jpg" class="img-fluid rounded-circle mb-3 mb-md-0"
-                                 alt="Foto von Cyrill Gurtner" style="width: 200px;">
-                        </div>
-                        <div class="col-12 col-sm-6 col-md-7">
-                            <p>
-                                Morbi quis auctor mauris, a ultrices ante.
-                                Sed libero ante, semper quis mollis non, fermentum at tellus. Sed nisl nulla, facilisis
-                                ac
-                                lobortis maximus, auctor eget justo. Mauris sit amet ipsum sed augue facilisis rhoncus
-                                vel
-                                at ex.
-                            </p>
-                        </div>
-                    </div>
                 </div>
             </div>
         </section>
