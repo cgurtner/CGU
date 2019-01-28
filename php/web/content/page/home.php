70b979be606c39425eac2ff85d740c5169e32c5d
Cyrill Gurtner <i@cygu.ch>
2019-01-28 22:30:45 +0100

    home?

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -3,32 +3,70 @@
 $dateNow = new DateTime();
 $birthday = new DateTime('1992-07-20');
 
-?><main class="fade-in">
-    <header>
-        <div class="bg">
-            <div class="pt-3" style="position: absolute; bottom: 0rem; left: 0; width: 100%; text-align: center; background-color: rgba(250, 250, 250, .5); height: 10rem;">
-                <img src="../../gui/image/logo-no-name.png" class="img-fluid"  style="height: 65%;"/>
-                <h1 style="letter-spacing: unset; font-size: 37px;"><span
-                            style="font-weight: 300;">Cyrill</span>&nbsp;<span
-                            style="font-weight: 600;">Gurtner</span></h1>
-            </div>
-        </div>
-    </header>
-    <div class="container mt-5">
-        <section>
-            <div class="row">
-                <div class="col-sm-12 col-md-10">
-                    <h2 class="text-center text-sm-left">Hi,</h2>
-                    <p>
-                        Vielen Dank, dass Du meine Webseite besuchst und Dich für meine Arbeit interessierst!
-                    </p>
-                    <p>
-                        Ich, Cyrill Gurtner, bin ein <?php echo $dateNow->diff($birthday)->y; ?> jähriger Softwareentwickler aus der Schweiz. Bereits
-                        früh entdeckte ich meine Affinität gegenüber der Softwareentwicklung. So kaufte ich mir mit 15 Jahren das ein Buch über die
-                        C++ Programmierung, mit speziellem Fokus auf die Spieleprogrammierung.
-                    </p>
+?>
+<main class="fade-in">
+    <section>
+        <header>
+            <div class="bg">
+                <div class="pt-3"
+                     style="position: absolute; bottom: 0rem; left: 0; width: 100%; text-align: center; background-color: rgba(250, 250, 250, .5); height: 10rem;">
+                    <img src="../../gui/image/logo-no-name.png" class="img-fluid" style="height: 65%;"/>
+                    <h1 style="letter-spacing: unset; font-size: 37px;"><span
+                                style="font-weight: 300;">Cyrill</span>&nbsp;<span
+                                style="font-weight: 600;">Gurtner</span></h1>
                 </div>
             </div>
-        </section>
-    </div>
+        </header>
+        <div class="container mt-5">
+            <section>
+                <header>
+                    <div class="row">
+                        <div class="col pt-5">
+                            <h2 class="h2-home text-center">Willkommen</h2>
+                            <hr/>
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb" vocab="http://schema.org/" typeof="BreadcrumbList">
+                                    <li class="breadcrumb-item active" property="itemListElement" typeof="ListItem">
+                                        <a property="item" typeof="WebPage" href="home"><span
+                                                    property="name">Home</span></a>
+                                        <meta property="position" content="1">
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </header>
+                <div class="row">
+                    <div class="col-sm-12 col-md-8">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et lorem consectetur,
+                        vehicula
+                        turpis et, egestas est. Nunc dignissim tortor quis nibh elementum, sed pretium tellus vulputate.
+                        Vivamus eget elementum lorem. Proin non ipsum imperdiet, porttitor erat ornare, hendrerit urna.
+                        Pellentesque fermentum eros vel turpis consequat, eu sollicitudin lorem egestas. Cras bibendum
+                        tortor ut semper molestie. Nulla et ipsum id sem gravida consequat. Morbi ac bibendum nibh.
+                        Vivamus
+                        sed tellus imperdiet, feugiat dui sed, blandit diam. Praesent ac nunc rutrum augue finibus
+                        vehicula.
+                        Maecenas eu molestie mi. Aenean a purus lectus. Phasellus enim diam, iaculis sit amet blandit
+                        non,
+                        luctus id urna.
+                    </div>
+                    <div class="col-sm-12 col-md-4">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et lorem consectetur,
+                        vehicula
+                        turpis et, egestas est. Nunc dignissim tortor quis nibh elementum, sed pretium tellus vulputate.
+                        Vivamus eget elementum lorem. Proin non ipsum imperdiet, porttitor erat ornare, hendrerit urna.
+                        Pellentesque fermentum eros vel turpis consequat, eu sollicitudin lorem egestas. Cras bibendum
+                        tortor ut semper molestie. Nulla et ipsum id sem gravida consequat. Morbi ac bibendum nibh.
+                        Vivamus
+                        sed tellus imperdiet, feugiat dui sed, blandit diam. Praesent ac nunc rutrum augue finibus
+                        vehicula.
+                        Maecenas eu molestie mi. Aenean a purus lectus. Phasellus enim diam, iaculis sit amet blandit
+                        non,
+                        luctus id urna.
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
 </main>
