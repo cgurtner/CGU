50ba3433a0d7509c191ca70c58937ca5f8b71a92
Cyrill Gurtner <i@cygu.ch>
2019-02-02 16:04:26 +0100

    add empty technologie page

---
+++ php/web/content/page/technologie.php
@@ -0,0 +1,9 @@
+<main class="fade-in">
+    <div class="container">
+        <section>
+            <?php include_once("content/comp/header.php"); ?>
+            <div class="row">
+            </div>
+        </section>
+    </div>
+</main>
