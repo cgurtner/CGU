70d70a8e22e6f0a8306bf9e7b8ca454aa7c31727
Cyrill Gurtner <i@cygu.ch>
2019-02-03 22:20:33 +0100

    homepage layout

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -39,11 +39,35 @@ $birthday = new DateTime('1992-07-20');
                 <div class="row">
                     <div class="col-sm-12 col-md-8">
                         <p>
-                            
+                            Suspendisse potenti. Duis libero erat, ultricies sed neque sed, suscipit gravida magna.
+                            Morbi ullamcorper lorem et rhoncus imperdiet. Sed eget sem porta, tempus leo eget, tincidunt
+                            velit. Morbi nec sollicitudin libero, ac tempor massa. Sed fringilla, odio pretium cursus
+                            sagittis, massa ante iaculis quam, sit amet sodales mi nunc malesuada lectus. Donec egestas
+                            lectus a sapien consectetur, ac maximus neque pharetra. Nullam in libero eu urna
+                            sollicitudin tempus vel eget sem. Sed vulputate, eros commodo volutpat maximus, nisi massa
+                            rutrum est, eu scelerisque augue lacus a ipsum. Nam venenatis nibh ac dui sagittis
+                            consequat. In posuere finibus malesuada. Aenean accumsan non tellus quis viverra. Aliquam
+                            vel efficitur lacus. Praesent rutrum lacus.
                         </p>
+                        <div class="row">
+                            <div class="col-sm-6 col-box-content mb-3">
+
+                            </div>
+                            <div class="col-sm-6 col-box-content mb-3">
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6 col-box-content mb-3">
+
+                            </div>
+                            <div class="col-sm-6 col-box-content mb-3">
+
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-sm-12 col-md-4 dark-column">
-                        <div style="padding: 1.5rem 3rem;" class="character">
+                    <div class="col-sm-12 col-md-4">
+                        <div class="character dark-column">
                             <header>
                                 <div style="text-align: center">
                                     <img src="../../gui/image/portrait.jpg" class="header-thumb"
@@ -60,10 +84,22 @@ $birthday = new DateTime('1992-07-20');
                                 <dt>Sprachen</dt>
                                 <dd>Deutsch ▪ Englisch C1</dd>
                                 <dt>Spezialisierung in...</dt>
-                                <dd><small>Web-Basierten Frontend und Backend Softwarelösungen.</small></dd>
-                                <dd><small>Interaktive UI Oberflächen mit JavaScript/React und anderen modernen libraries & frameworks.</small></dd>
-                                <dd><small>Implementation von REST API Schnittstellen und Bereitstellung zur Frontend-Anbindung.</small></dd>
-                                <dd><small>Anwenden und Entwerfen von relationalen Datenmodellen.</small></dd>
+                                <dd>
+                                    <small>Web-Basierten Frontend und Backend Softwarelösungen.</small>
+                                </dd>
+                                <dd>
+                                    <small>Interaktive UI Oberflächen mit JavaScript/React und anderen modernen
+                                        libraries & frameworks.
+                                    </small>
+                                </dd>
+                                <dd>
+                                    <small>Implementation von REST API Schnittstellen und Bereitstellung zur
+                                        Frontend-Anbindung.
+                                    </small>
+                                </dd>
+                                <dd>
+                                    <small>Anwenden und Entwerfen von relationalen Datenmodellen.</small>
+                                </dd>
                             </dl>
                         </div>
                     </div>
