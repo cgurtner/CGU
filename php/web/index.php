4ec8d96e3be7a2f22e18fa2c7f7f91e21a696ae9
Cyrill Gurtner <i@cygu.ch>
2019-02-17 17:54:29 +0100

    fix mobile-nav padding-top only on mobile

--- php/web/index.php
+++ php/web/index.php
@@ -47,7 +47,7 @@ if ($page['url'] === 'home') {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="<?php echo $page['meta']['description']; ?>">
-    <link rel="icon" href="">
+    <link rel="icon" href="/gui/image/logo-bw-no-name.png">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
     <link rel="stylesheet" href="/gui/css/fontawesome-all.min.css">
     <link rel="stylesheet" href="/gui/css/web-1.0.0.css?<?php echo uniqid(); ?>">
