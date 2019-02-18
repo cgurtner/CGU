3da09fa91d2231b7b566cae9abe9ce7690923600
Cyrill Gurtner <i@cygu.ch>
2019-02-18 21:02:49 +0100

    remove werdegang from home.php

--- php/web/content/page/home.php
+++ php/web/content/page/home.php
@@ -53,149 +53,27 @@ $age = $dateNow->diff($birthday)->y;
                                         Protokollführung".
                                         Mit den Technologien HTML/CSS, OO PHP, MySQL und LaTeX entwickelte ich ein
                                         webbasiertes Tool, um Meetings zu protokollieren, Teilnehmer einzuladen und
-                                        zu verwalten. Aus den Eingaben im Tool konnte anschliessend ein PDF generiert werden, welches
-                                        automatisch per E-Mail an alle Teilnehmer zur Einsicht freigegeben werden konnte.
+                                        zu verwalten. Aus den Eingaben im Tool konnte anschliessend ein PDF generiert
+                                        werden, welches
+                                        automatisch per E-Mail an alle Teilnehmer zur Einsicht freigegeben werden
+                                        konnte.
                                     </p>
                                     <p>
                                         Im Sommer 2013 schloss ich meine Lehre mit kantonaler Auszeichung ab.
                                     </p>
                                     <p>
                                         In meinem dritten Lehrjahr startete ich ein Praktikum bei der Datahouse AG im
-                                        Herzen von Zürich. Seit diesem Zeitpunkt beschäftige ich mich dort mit der Konzipierung von
-                                        Benutzeroberflächen, mit Softwarearchitekturen, mit dem Aufbau von komplexen Datenmodellen,
+                                        Herzen von Zürich. Seit diesem Zeitpunkt beschäftige ich mich dort mit der
+                                        Konzipierung von
+                                        Benutzeroberflächen, mit Softwarearchitekturen, mit dem Aufbau von komplexen
+                                        Datenmodellen,
                                         sowie mit dem Unterhalt von bestehenden Webapplikationen. Auch nach meinem
                                         Praktikum blieb ich der Datahouse AG treu und arbeite bis heute an
                                         spannenden Projekten verschiedenster Branchen.
                                     </p>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section>
-                        <div class="row">
-                            <div class="col-sm-12 col-box">
-                                <div>
-                                    <header>
-                                        <h2>Werdegang</h2>
-                                    </header>
-                                    <section>
-                                        <header>
-                                            <h3>Karriere</h3>
-                                        </header>
-                                        <section>
-                                            <header>
-                                                <h4 class="inline">Software Developer<br />
-                                                    <a href="https://www.datahouse.ch/" hreflang="de" target="_blank">
-                                                        Datahouse AG
-                                                    </a>
-                                                </h4>
-                                            </header>
-                                            <dl class="row">
-                                                <dt class="col-sm-4">Benutzeroberflächen</dt>
-                                                <dd class="col-sm-8">
-                                                    Techniche Konzipierung von Benutzeroberflächen mit modernen
-                                                    Technologien wie HTML5/CSS3 inkl. SASS sowie JavaScript und JQuery.<br />
-                                                    Implementation von modernen User-Interfaces in React in Verbindung
-                                                    mit diversen modernen Libraries.
-                                                </dd>
-                                                <dt class="col-sm-4">Softwarearchitektur</dt>
-                                                <dd class="col-sm-8">
-                                                    Implementation von komplexen Webapplikationen in PHP und Datenwebservices
-                                                    in C++. Planung und Modellierung von passenden relationalen
-                                                    Datenbankmodellen.
-                                                </dd>
-                                                <dt class="col-sm-4">Schnittstellen</dt>
-                                                <dd class="col-sm-8">
-                                                    Entwicklung von API Schnittstellen und Bereitstellung für die
-                                                    Anbindung an diverse Softwarelösungen. Implementationen
-                                                    in Python oder PHP.
-                                                </dd>
-                                                <dt class="col-sm-4">Betrieb & Support</dt>
-                                                <dd class="col-sm-8">
-                                                    Instandhaltung von Betriebsprojekten und Durchführen von
-                                                    Fehleranalysen. Wartung von bestehenden oder entwickelten
-                                                    Applikationen.
-                                                </dd>
-                                            </dl>
-                                        </section>
-                                    </section>
-                                    <section>
-                                        <header>
-                                            <h3>Ausbildung</h3>
-                                        </header>
-                                        <section>
-                                            <header>
-                                                <h4 class="inline">
-                                                    Hochschule für Technik Rapperswil<br />
-                                                    <a href="https://www.hsr.ch/" hreflang="de" target="_blank">hsr.ch</a>
-                                                </h4>
-                                            </header>
-                                            <dl class="row">
-                                            <dt class="col-sm-4">BSc FHO in Informatik</dt>
-                                            <dd class="col-sm-8">
-                                                Abschluss diverser Module in den Spezialisierungen
-                                                Application Design, Software Engineering und
-                                                Data Engineering.
-                                            </dd>
-                                            </dl>
-                                        </section>
-                                        <section>
-                                            <header>
-                                                <h4 class="inline">
-                                                    AKAD College<br />
-                                                    <a href="https://www.akad.ch/de-CH" hreflang="de" target="_blank">akad.ch</a>
-                                                </h4>
-                                            </header>
-                                            <dl class="row">
-                                            <dt class="col-sm-4">Eidg. Tech. Berufsmaturität</dt>
-                                            <dd class="col-sm-8">
-                                                Abschluss der technischen Berufsmaturität an der AKAD College
-                                                in Zürich, Oerlikon. Teilnahme im Teilzeitpensum und
-                                                parallele Weiteranstellung bei der Datahouse AG.
-                                            </dd>
-                                            </dl>
-                                            <dl class="row">
-                                                <dt class="col-sm-4">Schwerpunkt & Abschluss</dt>
-                                                <dd class="col-sm-8">
-                                                    Schwerpunktfächer der technischen Berufsmaturität sind Mathematik und
-                                                    die Naturwissenschaften Chemie & Physik. Abgschlossen habe ich
-                                                    mit der interdisziplinären Projektarbeit (IDPA) im Fach
-                                                    Physik.
-                                                </dd>
-                                            </dl>
-                                        </section>
-                                        <section>
-                                            <header>
-                                                <h4 class="inline">
-                                                    Informatikschule Olten<br/>
-                                                    <a href="https://www.informatikschuleolten.ch/" hreflang="de" target="_blank">informatikschuleolten.ch</a>
-                                                </h4>
-                                            </header>
-                                            <dl class="row">
-                                                <dt class="col-sm-4">Informatiker EFZ</dt>
-                                                <dd class="col-sm-8">
-                                                    Abschluss der vierjährigen Lehre zum Informatiker EFZ mit
-                                                    Fachrichtung Applikationsentwicklung. Ab dem dritten Lehrjahr
-                                                    Beginn meiner Anstellung bei der Datahouse AG.
-                                                </dd>
-                                            </dl>
-                                            <dl class="row">
-                                                <dt class="col-sm-4">Systemtechnik</dt>
-                                                <dd class="col-sm-8">
-                                                    Alle Module die im Rahmen der Fachrichtung Systemtechnik
-                                                    angeboten werden, habe ich ebenfalls besucht und
-                                                    bestanden.
-                                                </dd>
-                                            </dl>
-                                            <dl class="row">
-                                                <dt class="col-sm-4">IPA</dt>
-                                                <dd class="col-sm-8">
-                                                    Abschluss der Lehre mit der IPA "Web-Tool zur Protokollführung" mit
-                                                    sehr guter Note und kantonaler Auszeichnung.
-                                                </dd>
-                                            </dl>
-                                        </section>
-                                    </section>
+                                    <div class="text-right p-0">
+                                        <a href="werdegang">Zu meinem Werdegang</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
