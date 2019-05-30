092a97875d6a4384bd75aab03a225414e23747d7
Cyrill Gurtner <me@cgurtner.ch>
2019-05-30 23:12:34 +0200

    mv web-1.0.9.scss web-1.0.10.scss

--- php/web/index.php
+++ php/web/index.php
@@ -56,7 +56,7 @@ if ($page['url'] === 'home') {
     <link rel="icon" href="/gui/image/logo-favicon.jpg">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
     <link rel="stylesheet" href="/gui/css/fontawesome-all.min.css">
-    <link rel="stylesheet" href="/gui/css/web-1.0.9.css">
+    <link rel="stylesheet" href="/gui/css/web-1.0.10.css">
     <title><?php echo $pageTitle; ?></title>
 </head>
 <body <?php if ($page['url'] == 'home') {
