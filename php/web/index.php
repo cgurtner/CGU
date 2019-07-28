372d5c8e8b2b2ce2f4b4a6e5c39b41a9e8232aa3
Cyrill Gurtner <me@cgurtner.ch>
2019-07-28 19:28:11 +0200

    remove analytics from index.php

--- php/web/index.php
+++ php/web/index.php
@@ -37,18 +37,6 @@ if ($page['url'] === 'home') {
 ?><!DOCTYPE HTML>
 <html lang="de">
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134844121-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-        gtag('config', 'UA-134844121-1');
-    </script>
     <base href="<?php echo CONFIG['baseUrl']; ?>">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
