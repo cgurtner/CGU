020436abafbed5d79ae9298ce77da936ffba7fd7
Cyrill Gurtner <me@cgurtner.ch>
2020-07-01 23:26:30 +0200

    remove instagram feed

--- php/web/gui/js/web-1.0.10.js
+++ php/web/gui/js/web-1.0.10.js
@@ -7,13 +7,6 @@ $(document).ready(function () {
     var vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', vh + 'px');
 
-    const insta = $("img.insta-thumb");
-    const resizeInsta = function () {
-        insta.outerHeight(insta.outerWidth() * 1);
-    };
-    resizeInsta();
-    $(window).resize(resizeInsta);
-
     var t = 'm' + 'e';
     var y = 'cgu' + 'r' + 'tner'
     var a = '@';
