4e63e43636ff76ce2656b0945b9262ac231eb6fc
Cyrill Gurtner <me@cgurtner.ch>
2020-09-21 21:33:02 +0200

    new and minimalistic cv

--- php/web/content/page/werdegang.php
+++ php/web/content/page/werdegang.php
@@ -12,7 +12,7 @@
                             <div class="row">
                                 <div class="col-12 col-sm-4">
                                     <header>
-                                        <h3>Jan. 2020 - heute</h3>
+                                        <h3>2020 - heute</h3>
                                     </header>
                                 </div>
                                 <div class="col-12 col-sm-8">
@@ -21,11 +21,6 @@
                                             <h4 class="inline">Datahouse AG</h4>
                                             <h5>Senior Software Developer</h5>
                                         </header>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                        </p>
                                         <p>
                                             <a href="https://www.datahouse.ch/" target="_blank" rel="noreferrer">datahouse.ch</a>
                                         </p>
@@ -37,7 +32,7 @@
                             <div class="row">
                                 <div class="col-12 col-sm-4">
                                     <header>
-                                        <h3>Aug. 2019 - Sept. 2020</h3>
+                                        <h3>2019 - 2020</h3>
                                     </header>
                                 </div>
                                 <div class="col-12 col-sm-8">
@@ -46,11 +41,6 @@
                                             <h4 class="inline">Informatikschule Olten GmbH</h4>
                                             <h5>Informatikdozent</h5>
                                         </header>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                        </p>
                                         <p>
                                             <a href="https://www.informatikschuleolten.ch/home" target="_blank" rel="noreferrer">informatikschuleolten.ch</a>
                                         </p>
@@ -62,7 +52,7 @@
                             <div class="row">
                                 <div class="col-12 col-sm-4">
                                     <header>
-                                        <h3>Jul. 2013 - Dez. 2019</h3>
+                                        <h3>2013 - 2019</h3>
                                     </header>
                                 </div>
                                 <div class="col-12 col-sm-8">
@@ -71,11 +61,6 @@
                                             <h4 class="inline">Datahouse AG</h4>
                                             <h5>Software Developer</h5>
                                         </header>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                        </p>
                                         <p>
                                             <a href="https://www.datahouse.ch/" target="_blank" rel="noreferrer">datahouse.ch</a>
                                         </p>
@@ -87,7 +72,7 @@
                             <div class="row">
                                 <div class="col-12 col-sm-4">
                                     <header>
-                                        <h3>Sept. 2011 - Jul. 2013</h3>
+                                        <h3>2011 - 2013</h3>
                                     </header>
                                 </div>
                                 <div class="col-12 col-sm-8">
@@ -96,11 +81,6 @@
                                             <h4 class="inline">Datahouse AG</h4>
                                             <h5>Praktikant</h5>
                                         </header>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                        </p>
                                         <p>
                                             <a href="https://www.datahouse.ch/" target="_blank" rel="noreferrer">datahouse.ch</a>
                                         </p>
@@ -121,7 +101,7 @@
                             <div class="row">
                                 <div class="col-12 col-sm-4">
                                     <header>
-                                        <h3>Sept. 2016 - Sept. 2018</h3>
+                                        <h3>2016 - 2018</h3>
                                     </header>
                                 </div>
                                 <div class="col-12 col-sm-8">
@@ -130,11 +110,6 @@
                                             <h4 class="inline">Hochschule für Technik Rapperswil</h4>
                                             <h5>BSc FHO in Informatik</h5>
                                         </header>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                        </p>
                                         <p>
                                             <a href="https://www.hsr.ch/" target="_blank" rel="noreferrer">hsr.ch</a>
                                         </p>
@@ -146,7 +121,7 @@
                             <div class="row">
                                 <div class="col-12 col-sm-4">
                                     <header>
-                                        <h3>Aug. 2013 - Feb. 2015</h3>
+                                        <h3>2013 - 2015</h3>
                                     </header>
                                 </div>
                                 <div class="col-12 col-sm-8">
@@ -154,11 +129,6 @@
                                         <h4 class="inline">AKAD College</h4>
                                         <h5>Eidg. Tech. Berufsmaturität</h5>
                                     </header>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                                        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                    </p>
                                     <p>
                                         <a href="https://www.akad.ch/" target="_blank" rel="noreferrer">akad.ch</a>
                                     </p>
@@ -169,7 +139,7 @@
                             <div class="row">
                                 <div class="col-12 col-sm-4">
                                     <header>
-                                        <h3>Jul. 2009 - Jul. 2013</h3>
+                                        <h3>2009 - 2013</h3>
                                     </header>
                                 </div>
                                 <div class="col-12 col-sm-8">
@@ -177,11 +147,6 @@
                                         <h4 class="inline">Informatikschule Olten GmbH</h4>
                                         <h5>Applikationsentwickler EFZ</h5>
                                     </header>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                                        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                                    </p>
                                     <p>
                                         <a href="https://www.informatikschuleolten.ch/" target="_blank" rel="noreferrer">informatikschuleolten.ch</a>
                                     </p>
@@ -208,7 +173,36 @@
                                     <section>
                                         <header>
                                             <h4 class="inline">React Advanced</h4>
-                                            <h5>London, GB</h5>
+                                        </header>
+                                        <p>
+                                            London, GB
+                                        </p>
+                                    </section>
+                                    <section>
+                                        <header>
+                                            <h4 class="inline">C1 English for the workplace</h4>
+                                            <p>
+                                                Flying Teachers GmbH, Zürich, CH
+                                            </p>
+                                        </header>
+                                    </section>
+                                </div>
+                            </div>
+                        </section>
+                        <section>
+                            <div class="row">
+                                <div class="col-12 col-sm-4">
+                                    <header>
+                                        <h3>2017</h3>
+                                    </header>
+                                </div>
+                                <div class="col-12 col-sm-8">
+                                    <section>
+                                        <header>
+                                            <h4 class="inline">Survival Guide Selbstständigkeit</h4>
+                                            <p>
+                                                Business-Tools AG, ETH Zürich, CH
+                                            </p>
                                         </header>
                                     </section>
                                 </div>
@@ -225,7 +219,9 @@
                                     <section>
                                         <header>
                                             <h4 class="inline">JavaScript Days</h4>
-                                            <h5>Berlin, DE</h5>
+                                            <p>
+                                                Berlin, DE
+                                            </p>
                                         </header>
                                     </section>
                                 </div>
@@ -242,7 +238,9 @@
                                     <section>
                                         <header>
                                             <h4 class="inline">Norwegian Developers Conference (NDC)</h4>
-                                            <h5>Oslo, NO</h5>
+                                            <p>
+                                                Oslo, NO
+                                            </p>
                                         </header>
                                     </section>
                                 </div>
