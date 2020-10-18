7f87f4e09a82a0c255eaab65f4186c016cc96759
Cyrill Gurtner <me@cgurtner.ch>
2020-10-18 13:11:28 +0200

    Revert "remove socials, because I am not social anymore. There is an end for everything."
    This reverts commit 8377af39216e007416a1dacd0882198f649f7fbb.

--- php/web/config/.docker.config.php
+++ php/web/config/.docker.config.php
@@ -9,6 +9,20 @@ $CONFIG = array(
     'email.smtp.host' => 'antiqua.sui-inter.net',
     'email.smtp.user' => 'no-reply@cgurtner.ch',
     'email.smtp.password' => 'cBac%a2.A?cJAHcYATC23@cAB',
+    'socials' => array(
+        'gitlab' => array(
+            'name' => 'gitlab',
+            'url' => 'https://gitlab.com/cgurtner.ch',
+            'icon' => 'fa-gitlab',
+            'title' => 'GitLab'
+        ),
+        'instagram' => array(
+            'name' => 'instagram',
+            'url' => 'https://www.instagram.com/cgurtner.ch',
+            'icon' => 'fa-instagram',
+            'title' => 'Instagram'
+        )
+    ),
     'recaptcha-client-key' => '6LcOZaYUAAAAAKrMyY86NZuWOT4lECDilAV4YF0c',
     'recaptcha-secret-key' => '6LcOZaYUAAAAABN9bPCddtq8O2C5S1vR3gxHwhD5'
 );
