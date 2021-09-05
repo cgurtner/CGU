52cd560b9afeb84c7d83ed12cde69a6facf3bc6a
Cyrill Gurtner <me@cgurtner.ch>
2021-09-05 18:45:13 +0200

    added LinkedIn to socials

--- php/web/config/.docker.config.php
+++ php/web/config/.docker.config.php
@@ -9,7 +9,14 @@ $CONFIG = array(
     'email.smtp.host' => 'antiqua.sui-inter.net',
     'email.smtp.user' => 'no-reply@cgurtner.ch',
     'email.smtp.password' => 'cBac%a2.A?cJAHcYATC23@cAB',
-    'socials' => array(),
+    'socials' => array(
+        'linkedin' => array(
+            'name' => 'linkedin',
+            'url' => 'https://linkedin.com/in/cgurtner',
+            'class' => 'fab fa-linkedin',
+            'title' => 'LinkedIn'
+        )
+    ),
     'recaptcha-client-key' => '6LcOZaYUAAAAAKrMyY86NZuWOT4lECDilAV4YF0c',
     'recaptcha-secret-key' => '6LcOZaYUAAAAABN9bPCddtq8O2C5S1vR3gxHwhD5'
 );
