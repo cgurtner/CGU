32ae1f40a5ffdc11feb37c293c554d0840523c08
Cyrill Gurtner <me@cgurtner.ch>
2022-07-13 17:10:41 +0200

    remove unused config variables

--- php/web/config/.docker.config.php
+++ php/web/config/.docker.config.php
@@ -3,12 +3,6 @@
 $CONFIG = array(
     'baseUrl' => 'http://localhost:8000/',
     'baseUrlAsShown' => 'localhost:8000',
-    'email.to.address' => 'me@cgurtner.ch',
-    'email.from.address' => 'no-reply@cgurtner.ch',
-    'email.from.name' => 'Cyrill Gurtner',
-    'email.smtp.host' => 'antiqua.sui-inter.net',
-    'email.smtp.user' => 'no-reply@cgurtner.ch',
-    'email.smtp.password' => 'cBac%a2.A?cJAHcYATC23@cAB',
     'socials' => array(
         'linkedin' => array(
             'name' => 'linkedin',
