8700f3396fc3a7560845d7505beed129652c11e4
Cyrill Gurtner <me@cgurtner.ch>
2022-07-14 00:34:12 +0200

    rename release docker images

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-rm -rf dist
-docker rm cgu_web_prod
+docker build --target=prod -t cgu_web:release ./php/web/ 
+docker create -ti --name cgu_web-release cgu_web:release bash
+docker cp cgu_web-release:/var/www/dist cgu_web-release
 
-docker build --target=prod -t cgu_web:latest ./php/web/ 
-docker create -ti --name cgu_web_prod cgu_web:latest bash
-docker cp cgu_web_prod:/var/www/dist dist
+rsync -e 'ssh -p2121' -rvP --delete cgu_web-release/ cgu@cgurtner.ch:/httpdocs/cgurtner.ch/
 
-rsync -e 'ssh -p2121' -rvP --delete dist/ cgu@cgurtner.ch:/httpdocs/cgurtner.ch/
-
-docker rm cgu_web_prod
-rm -rf dist
+docker rm cgu_web-release
+docker rmi cgu_web:release
+rm -rf cgu_web-release
